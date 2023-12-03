460377947a3306750d50a622d751e9f6f915a1bc
Ali Pardeshenas <a.pardeshenas@gmail.com>
2023-12-03 18:11:48 +0330

    Add chart to dashboard page

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,8 +1,62 @@
+"use client";
 import React from "react";
+import moment from "moment";
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
 
 import "./dashboard.scss";
 
+Chart.register(...registerables);
+
+const options = {
+    plugins: {
+        title: {
+            display: true,
+            text: "Sales amount in last 7 days",
+        },
+    },
+    responsive: true,
+    scales: {
+        x: {
+            stacked: true,
+            grid: {
+                display: false,
+            },
+        },
+        y: {
+            stacked: true,
+            grid: {
+                display: false,
+            },
+        },
+    },
+};
+
 const Dashboard = () => {
+    let last7Days = [];
+    let currentDate = moment();
+    for (let i = 0; i < 7; i++) {
+        // Subtract 'i' days from the current date
+        let date = currentDate.clone().subtract(i, "days");
+
+        // Format the date in MM/dd format and push it to the array
+        last7Days.push(date.format("MM-DD"));
+    }
+    const labels = last7Days.reverse();
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: "Sales amount ($)",
+                data: [12, 15, 3, 5, 2, 10, 7],
+                fill: false,
+                borderColor: "rgb(75, 192, 192)",
+                tension: 0.1,
+            },
+        ],
+    };
+
     return (
         <main className="content">
             <h4>Dashboard</h4>
@@ -13,7 +67,17 @@ const Dashboard = () => {
                 <div className="card"></div>
             </div>
             <div className="dashboard-chart-section">
-                <div className="card"></div>
+                <div className="card">
+                    <div className="chart">
+                        <Line
+                            options={options}
+                            data={data}
+                            style={{
+                                padding: "20px",
+                            }}
+                        />
+                    </div>
+                </div>
                 <div className="card"></div>
             </div>
         </main>
